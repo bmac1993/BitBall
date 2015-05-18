94447fb1c163d5c610208884a9873433468eb9c6
Bmac1993 <Bmac1993@gmail.com>
2015-05-17 21:39:47 -0400

    fixed data issue

--- client/js/login.js
+++ client/js/login.js
@@ -4,7 +4,7 @@ var login = {
         $('.loginMsg').hide();
         var username = document.getElementById("username").value;
         var password = document.getElementById("password").value;
-
+        var data = {'username':username, 'password':password };
         socket.emit('login', data);
 
         // if returns an object -> set user to returned object
